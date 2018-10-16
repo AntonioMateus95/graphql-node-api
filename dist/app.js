425d0b95e297edae5867e8ae85c4202cd575a4ef
AntonioMateus95 <antoniocmsilva95@gmail.com>
2018-10-15 23:55:56 -0300

    express and event handlers are now working properly

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//o alias abaixo é um nome para o módulo sendo importado
+const express = require("express");
+class App {
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+    middleware() {
+        //o método use serve para todos os tipos de requisição
+        this.express.use('/hello', (req, res, next) => {
+            res.send({
+                hello: 'Hello world!'
+            });
+        });
+    }
+}
+//exportar uma instãncia da aplicação Express
+exports.default = new App().express;
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log('Hello world!');
+//o alias abaixo é um nome para o módulo sendo importado
+import * as express from 'express';
+
+class App {
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+
+    private middleware() : void {
+        //o método use serve para todos os tipos de requisição
+        this.express.use('/hello', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+            res.send({
+                hello: 'Hello world!'
+            })
+        })
+    }
+}
+
+//exportar uma instãncia da aplicação Express
+export default new App().express;
