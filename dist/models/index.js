9183cffef23800fda7c45aacb0f9d036b718918b
AntonioMateus95 <antoniocmsilva95@gmail.com>
2018-10-19 15:58:21 -0300

    index.ts now is ready to configure sequelize for us

--- dist/models/index.js
+++ dist/models/index.js
@@ -1,10 +1,43 @@
 "use strict";
 //responsável por ver quais models existem no nosso projeto
 //e por fazer a comunicação com o sequelize, para posteriormente
-//criar o banco de dados e as tabelas
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs"); //file system
 const path = require("path");
+const Sequelize = require("sequelize");
 //"module" é uma das variáveis que o Node.js disponibiliza
 //dentro do escopo do módulo em que estamos
 const basename = path.basename(module.filename);
-console.log(module.filename);
+//ambiente que é passado na chamada do nodemon
+const env = process.env.NODE_ENV || 'development';
+//o require pega o conteúdo do json cujo caminho foi passado por parâmetro
+//e o transforma em um objeto 
+let config = require(path.resolve(`${__dirname}./../config/config.json`))[env];
+//conexão com o banco de dados
+let db = null;
+//precisamos garantir que estamos trabalhando com uma única 
+//instância do banco de dados
+if (!db) {
+    db = {};
+    //já preparamos o sequelize para trabalhar com o banco de dados Mysql
+    const sequelize = new Sequelize(config.database, config.username, config.password, config);
+    //agora precisamos das models
+    fs.readdirSync(__dirname)
+        .filter((file) => {
+        //apenas arquivos com nome válidos, com extensão .js e diferentes do arquivo index.js em que estamos
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+        .forEach((file) => {
+        //para importar um model no sequelize, usamos o método "import"
+        const model = sequelize.import(path.join(__dirname, file));
+        db[model['name']] = model;
+    });
+    Object.keys(db).forEach((modelName) => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
+    });
+    //o atributo a seguir vai servir pra sincronizar o sequelize com o MySQL posteriormente
+    db['sequelize'] = sequelize;
+}
+exports.default = db;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,56 @@
 //responsável por ver quais models existem no nosso projeto
 //e por fazer a comunicação com o sequelize, para posteriormente
-//criar o banco de dados e as tabelas
 
 import * as fs from 'fs'; //file system
 import * as path from 'path';
 import * as Sequelize from 'sequelize';
 
+import { DbConnection } from '../interfaces/DbConnectionInterface';
+
 //"module" é uma das variáveis que o Node.js disponibiliza
 //dentro do escopo do módulo em que estamos
 const basename: string = path.basename(module.filename);
-console.log(module.filename);
+//ambiente que é passado na chamada do nodemon
+const env: string = process.env.NODE_ENV || 'development';
+//o require pega o conteúdo do json cujo caminho foi passado por parâmetro
+//e o transforma em um objeto 
+let config = require(path.resolve(`${__dirname}./../config/config.json`))[env];
+//conexão com o banco de dados
+let db = null;
+
+//precisamos garantir que estamos trabalhando com uma única 
+//instância do banco de dados
+if (!db) {
+    db = {};
+    
+    //já preparamos o sequelize para trabalhar com o banco de dados Mysql
+    const sequelize: Sequelize.Sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config
+    );
+
+    //agora precisamos das models
+    fs.readdirSync(__dirname)
+        .filter((file: string) => {
+            //apenas arquivos com nome válidos, com extensão .js e diferentes do arquivo index.js em que estamos
+            return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+        })
+        .forEach((file: string) => {
+            //para importar um model no sequelize, usamos o método "import"
+            const model = sequelize.import(path.join(__dirname, file));
+            db[model['name']] = model;
+        });
+
+    Object.keys(db).forEach((modelName: string) => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
+    });
+
+    //o atributo a seguir vai servir pra sincronizar o sequelize com o MySQL posteriormente
+    db['sequelize'] = sequelize; 
+}
+
+export default <DbConnection>db;
