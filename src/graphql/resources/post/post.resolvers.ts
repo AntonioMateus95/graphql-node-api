094dc209f7f6d8790839b58d9b0c16aab2e78318
Antonio <antonio.silva@madeinweb.com.br>
2018-11-05 14:40:55 -0200

    Post mutations' resolvers implemented

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -1,3 +1,4 @@
+import { Transaction } from 'sequelize';
 import { DbConnection } from './../../../interfaces/DbConnectionInterface';
 import { GraphQLResolveInfo } from 'graphql';
 import { PostInstance } from './../../../models/PostModel';
@@ -37,5 +38,41 @@ export const postResolvers = {
                     return post;
                 });
         }, 
+    },
+
+    Mutation: {
+
+        createPost: (parent, { input }, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                    .create(input, { transaction: t });
+            });
+        },
+
+        updatePost: (parent, { id, input }, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id);
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                    .findById(id)
+                    .then((post: PostInstance) => {
+                        if (!post) throw new Error(`Post with id ${id} not found!`);
+                        return post.update(input, { transaction: t });
+                    })
+            });
+        },
+
+        deletePost: (parent, { id }, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            id = parseInt(id)
+            return db.sequelize.transaction((t: Transaction) => {
+                return db.Post
+                .findById(id)
+                .then((post: PostInstance) => {
+                    if (!post) throw new Error(`Post with id ${id} not found!`);
+                    return post.destroy({ transaction: t })
+                        .then(post => !!post);
+                });
+            });
+        }
+
     }
 }
