2a8d40f9c0b3eb3aa41e61a9fc0e396bc24c0f3d
Antonio <antonio.silva@madeinweb.com.br>
2018-11-05 15:42:56 -0200

    Resolvers added to executable schema: now we can test our API

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,4 +1,5 @@
 import { makeExecutableSchema } from 'graphql-tools';
+import { merge } from 'lodash';
 
 import {Query} from './query';
 import {Mutation} from './mutation';
@@ -6,6 +7,14 @@ import {Mutation} from './mutation';
 import { commentTypes } from './resources/comment/comment.schema';
 import { postTypes } from './resources/post/post.schema';
 import { userTypes } from './resources/user/user.schema';
+
+import { commentResolvers } from './resources/comment/comment.resolvers';
+import { postResolvers } from './resources/post/post.resolvers';
+import { userResolvers } from './resources/user/user.resolvers';
+
+//precisamos de uma forma de mesclar todas as queries e mutations feitas em arquivos separados:
+//para isso usamos uma função do lodash chamada merge. Como o próprio nome já diz, ela pega os atributos iguais de uma série de objetos e mescla os seus conteúdos
+const resolvers = merge(commentResolvers, postResolvers, userResolvers);
 
 const SchemaDefinition = `
     type Schema {
@@ -22,5 +31,6 @@ export default makeExecutableSchema({
         commentTypes,
         postTypes,
         userTypes
-    ]
+    ],
+    resolvers
 });
